901aa614e675cf30cd5555aa6413918630a77ac8
Yuri Brigido Ferreira <37347459+yuri-ferreira@users.noreply.github.com>
2025-06-24 13:47:35 -0300

    userdata

---
+++ user_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum update -y
+yum install -y httpd
+echo "Hello World" > /var/www/html/index.html
+ 
+# Servidor básico com endpoint /teste
+cat <<EOL > /var/www/html/teste
+#!/bin/bash
+echo "Content-type: text/plain"
+echo ""
+echo "Requisição recebida em $(hostname)"
+sleep 5  # Simula carga
+EOL
+ 
+chmod +x /var/www/html/teste
+systemctl start httpd
+systemctl enable httpd
